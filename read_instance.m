5d2201915addfca0d04fa1e255c91e8965b19325
Jorge <jorge.reta.carcamo@gmail.com>
2018-03-05 20:05:39 +0100

    First commit

---
+++ read_instance.m
@@ -0,0 +1,39 @@
+%
+% function y = read_instance(fname,decimate)
+% Reads an instance for training/classification purposes and decimates it. 
+% Instance can be a video file or a text file containing ground truth as as
+% single character array with the corresponding label for each frame
+% Input: fname - file name and path
+%        decimate - decimation value of the read video frames
+% Output:matrix/array containing the instance data
+function y = read_instance(fname, decimate)
+
+[~,name,ext] = fileparts(fname);
+
+if strcmp(ext, '.avi')
+    % File is a video file
+    v = VideoReader(fname);
+    L = v.Duration * v.FrameRate; % number of video frames
+    video = zeros(v.Height, v.Width, 3, ceil(L/decimate), 'uint8');
+    i = 0;
+    while hasFrame(v)
+        frame = readFrame(v);
+        i = i+1;        
+        % Decimate video to 1/10
+        if ~mod(i-1, decimate)
+            video(:,:,:,(i-1)/decimate+1) = frame;
+        end
+    end
+    y = video; %output
+elseif strcmp(ext, '.txt')
+    text = fileread(fname);
+    L = length(text); % number of video frames
+    labels = zeros(1,ceil(L/decimate));
+    for i=1:length(text)
+        if ~mod(i-1,decimate)
+            labels((i-1)/decimate+1) = str2double(text(i));
+        end
+    end
+    y = labels; % output
+end
+end
