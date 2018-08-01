9e7ee91b65611c0d830a65a0953be3f9389b160b
jretac <jorge.reta.carcamo@gmail.com>
2018-08-01 19:51:14 +0200

    Change output structure field names

--- ReduceDataset.m
+++ ReduceDataset.m
@@ -1,5 +1,5 @@
 function dataset_reduced = ReduceDataset(dataset, pca_struct, pca_ratio)
-
+%PCA projection and dimensionality reduction retaining the 100*pca_ratio variance
 L = length(dataset);
 for i=1:length(pca_struct)
     mu = pca_struct(i).mu;
@@ -10,10 +10,10 @@ for i=1:length(pca_struct)
     cumVariance = find(variance > pca_ratio);
     numComponentsPCA = cumVariance(1);
     
-    descriptors = dataset(mod(i-1,L)+1).Descriptors;
+    descriptors = dataset(mod(i-1,L)+1).descriptors;
     descriptors = (descriptors - mu) * coeff;
     descriptors = descriptors(:,1:numComponentsPCA);
-    labels = dataset(mod(i-1,L)+1).Labels;
+    labels = dataset(mod(i-1,L)+1).labels;
     
     dataset_reduced(i).situation = dataset(mod(i-1,L)+1).situation;
     dataset_reduced(i).descriptors = descriptors;
